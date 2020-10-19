4cd85fa053a6f8c1260d2d857ad92b7a5fd65e7b
runshine <woshiyuhao0819@gmail.com>
2020-10-19 07:06:55 -0700

    begin repeater test v0.42

--- docker/restrict_image_build_script/resource/docker_start.sh
+++ docker/restrict_image_build_script/resource/docker_start.sh
@@ -7,7 +7,7 @@
 python3 /root/io_decrypt_server.py IO_DECRYPT_LISTEN_HOST IO_DECRYPT_LISTEN_PORT IO_DECRYPT_UPSTREAM_HOST IO_DECRYPT_UPSTREAM_PORT
 
 #python3 /root/analysis_server.py  ANALYSIS_SERVER_HOST ANALYSIS_SERVER_PORT /root/  /root/input_elf /root/libc.so 0
-#python3 verify.py  /root/ /root/input_elf /root/libc.so
+#python3 verify.py  /root/ /root/input_elf /root/libc.so  #we must start tmux before this command
 #python3 repeater.py  /root/ /root/input_elf
 
 sleep infinity;
